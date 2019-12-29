3846c58b9ce9a5f8fabcb56acaccc5506ee05670
Peter Lesslie <pclesslie@gmail.com>
2019-12-28 20:30:46 -0600

    Initial vector implementation

---
+++ include/pltables++/vector.h
@@ -0,0 +1,119 @@
+#pragma once
+
+#include <cstdlib>
+#include <cstring>
+#include <type_traits>
+
+namespace plt {
+
+template <class T>
+class Vector
+{
+public:
+    using value_type = T;
+
+    constexpr Vector() noexcept = default;
+
+    Vector(int size, T elem = T()) noexcept : _size{ size }, _asize{ size }
+    {
+        _data = static_cast<T*>(calloc(sizeof(*_data), _asize));
+        for (int i = 0; i < _size; ++i) {
+            new (&_data[i]) T{ elem };
+        }
+    }
+
+    Vector(const Vector& other) noexcept : _size{ other._size },
+                                           _asize{ other._asize }
+    {
+        _data = static_cast<T*>(calloc(sizeof(*_data), _asize));
+        // TODO: work for non trivial types
+        memcpy(_data, other._data, sizeof(*_data) * _size);
+    }
+
+    Vector(Vector&& other) noexcept : _data{ other._data },
+                                      _size{ other._size },
+                                      _asize{ other._asize }
+    {
+        other._data = nullptr;
+        other._size = other._asize = 0;
+    }
+
+    Vector& operator=(const Vector& other) noexcept
+    {
+        free(_data);
+        _size = other._size;
+        _asize = other._asize;
+        _data = static_cast<T*>(calloc(sizeof(*_data), other._asize));
+        // TODO: work for non trivial types
+        memcpy(_data, other._data, sizeof(*_data) * _size);
+        return *this;
+    }
+
+    Vector& operator=(Vector&& other) noexcept
+    {
+        free(_data);
+        _size = other._size;
+        other._size = 0;
+        _asize = other._asize;
+        other._asize = 0;
+        _data = other._data;
+        other._data = nullptr;
+        return *this;
+    }
+
+    ~Vector() noexcept
+    {
+        free(_data);
+        _size = _asize = 0;
+    }
+
+    void append(const T& x) noexcept
+    {
+        if (_size == _asize)
+            _grow(1.5 * _asize + 4);
+        new (&_data[_size++]) T{ x };
+    }
+
+    void pop() noexcept
+    {
+        assert(!is_empty());
+        _data[--_size].~T();
+    }
+
+    void clear() noexcept
+    {
+        free(_data);
+        _size = _asize = 0;
+    }
+
+    void shrink_to_fit() noexcept { _grow(_size); }
+    constexpr T& operator[](size_t i) noexcept { return _data[i]; }
+    constexpr T& operator[](size_t i) const noexcept { return _data[i]; }
+    constexpr bool is_empty() const noexcept { return _size == 0; }
+    constexpr int size() const noexcept { return _size; }
+    constexpr int capacity() const noexcept { return _asize; }
+
+private:
+    void _grow(int newasize) noexcept
+    {
+        assert(newasize >= _size);
+        if
+            constexpr(std::is_trivially_copyable_v<T>)
+            {
+                _data =
+                  static_cast<T*>(realloc(_data, newasize * sizeof(*_data)));
+            }
+        else {
+            // TODO: work for non-trivial types
+            throw 42;
+        }
+        _asize = newasize;
+    }
+
+private:
+    value_type* _data = nullptr;
+    int _size = 0;
+    int _asize = 0;
+};
+
+} // ~plt
---
+++ tests/test_vector.cpp
@@ -0,0 +1,126 @@
+#include <catch2/catch.hpp>
+#include <pltables++/vector.h>
+
+using namespace plt;
+
+TEST_CASE("Vector - construction")
+{
+    SECTION("Default construct empty vector")
+    {
+        Vector<int> vec;
+        REQUIRE(vec.size() == 0);
+        REQUIRE(vec.capacity() == 0);
+    }
+
+    SECTION("Default construct N IsTrivial elements")
+    {
+        Vector<int> vec{ 6, 42 };
+        REQUIRE(vec.size() == 6);
+        REQUIRE(vec.capacity() >= 6);
+        for (int i = 0; i < vec.size(); ++i) {
+            REQUIRE(vec[i] == 42);
+        }
+
+        const Vector<int>& cvec = vec;
+        for (int i = 0; i < cvec.size(); ++i) {
+            REQUIRE(cvec[i] == 42);
+        }
+    }
+
+    SECTION("Copy constructor")
+    {
+        Vector<int> v1{6, 44};
+        Vector<int> v2{v1};
+        REQUIRE(v1.size() == 6);
+        REQUIRE(v2.size() == 6);
+        for (int i = 0; i < v2.size(); ++i) {
+            REQUIRE(v2[i] == 44);
+            REQUIRE(v1[i] == 44);
+        }
+    }
+
+    SECTION("Move constructor")
+    {
+        Vector<int> v1{6, 44};
+        Vector<int> v2{std::move(v1)};
+        REQUIRE(v1.size() == 0);
+        REQUIRE(v2.size() == 6);
+        for (int i = 0; i < v2.size(); ++i) {
+            REQUIRE(v2[i] == 44);
+        }
+    }
+
+    SECTION("Copy assignment")
+    {
+        Vector<int> v1{5, 41};
+        Vector<int> v2{10, 1};
+
+        REQUIRE(v1.size() == 5);
+        REQUIRE(v2.size() == 10);
+        REQUIRE(v1[0] == 41);
+        REQUIRE(v2[0] == 1);
+
+        v2 = v1;
+        REQUIRE(v1.size() == 5);
+        REQUIRE(v2.size() == 5);
+        for (int i = 0; i < v2.size(); ++i) {
+            REQUIRE(v2[i] == 41);
+            REQUIRE(v1[i] == 41);
+        }
+    }
+
+    SECTION("Move assignment")
+    {
+        Vector<int> v1{5, 41};
+        Vector<int> v2{10, 1};
+
+        REQUIRE(v1.size() == 5);
+        REQUIRE(v2.size() == 10);
+        REQUIRE(v1[0] == 41);
+        REQUIRE(v2[0] == 1);
+
+        v2 = std::move(v1);
+        REQUIRE(v1.size() == 0);
+        REQUIRE(v2.size() == 5);
+        for (int i = 0; i < v2.size(); ++i) {
+            REQUIRE(v2[i] == 41);
+        }
+    }
+}
+
+TEST_CASE("Vector - append")
+{
+    Vector<int> vec;
+    constexpr int N = 1024;
+    REQUIRE(vec.is_empty() == true);
+    for (int i = 0; i < N; ++i) {
+        REQUIRE(vec.size() == i);
+        vec.append(i);
+    }
+    REQUIRE(vec.size() == N);
+
+    for (int i = 0; i < N; ++i) {
+        REQUIRE(vec[i] == i);
+    }
+
+    for (int i = 0; i < N/2; ++i) {
+        vec.pop();
+    }
+    REQUIRE(vec.size() == N/2);
+
+    for (int i = 0; i < N/2; ++i) {
+        vec.append(N/2 + i + 1);
+    }
+    REQUIRE(vec.size() == N);
+
+    for (int i = 0; i < N; ++i) {
+        if (i < N/2) {
+            REQUIRE(vec[i] == i);
+        } else {
+            REQUIRE(vec[i] == i + 1);
+        }
+    }
+
+    vec.shrink_to_fit();
+    REQUIRE(vec.size() == vec.capacity());
+}
